d007ffaeb128126e0ec0928ce0c2271a54ca2895
Dawid Malik <dawid.malik.it@gmail,com$  git config --global user.email dawid.malik.it@gmail.com>
2025-06-26 21:32:43 +0200

    zaległe zmiany

--- js/main.js
+++ js/main.js
@@ -6,11 +6,9 @@ const allNavItems = document.querySelectorAll('.nav__link')
 var prevScrollpos = window.scrollY
 const msgStatus = document.querySelector('.msg-status')
 
-
-
 // mail/////
 
-console.log(document.location.search);
+console.log(document.location.search)
 
 if (document.location.search === '?mail_status=sent') {
 	msgStatus.classList.add('succes')
@@ -18,51 +16,50 @@ if (document.location.search === '?mail_status=sent') {
 
 	setTimeout(() => {
 		msgStatus.classList.remove('succes')
-	}, 3000);
+	}, 3000)
 }
 
-if (document.location.search === '?mail_status=error'){
+if (document.location.search === '?mail_status=error') {
 	msgStatus.classList.add('error')
 	msgStatus.textContent = 'Coś poszło nie tak. Spróbuj ponownie.'
 
 	setTimeout(() => {
 		msgStatus.classList.remove('error')
-	}, 3000);
+	}, 3000)
 }
 ////////
 
-
-
 // znikanie i pojawianie się nawigacji oraz buttona na scrollu
 
-window.onscroll = function() {onscroll_function()};
-const navbar = document.getElementById("nav-desktop");
-const sticky = navbar.offsetTop;
+window.onscroll = function () {
+	onscroll_function()
+}
+const navbar = document.getElementById('nav-desktop')
+const sticky = navbar.offsetTop
 function onscroll_function() {
-   navFunction();
-   scrollFunction();
-  
+	navFunction()
+	scrollFunction()
 }
 
 function navFunction() {
 	var currentScrollPos = window.scrollY
 	if (prevScrollpos > currentScrollPos) {
-		  document.getElementById('nav-desktop').style.top = '0'
+		document.getElementById('nav-desktop').style.top = '0'
 	} else {
-		  document.getElementById('nav-desktop').style.top = '-7em'
+		document.getElementById('nav-desktop').style.top = '-7em'
 	}
-	  prevScrollpos = currentScrollPos
+	prevScrollpos = currentScrollPos
 }
 function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("topBtn").style.display = "block";
-    } else {
-        document.getElementById("topBtn").style.display = "none";
-    }
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		document.getElementById('topBtn').style.display = 'block'
+	} else {
+		document.getElementById('topBtn').style.display = 'none'
+	}
 }
 function topFunction() {
-    document.body.scrollTop = 0; 
-    document.documentElement.scrollTop = 0; 
+	document.body.scrollTop = 0
+	document.documentElement.scrollTop = 0
 }
 
 ///////
@@ -82,18 +79,8 @@ const handleNav = () => {
 navBtn.addEventListener('click', handleNav)
 //////
 
-
-
-
-
-
-
-
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
 }
 handleCurrentYear()
-
-
-
